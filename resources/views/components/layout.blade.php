e6fb3f77e2b5028410f373b8e153ae3d6e089fbb
Emmanuel Oduro <emmarthurson@gmail.cdom>
2026-03-19 10:14:43 +0000

    feat: Add base Blade layout component and configure site-wide crawling restrictions with robots.txt.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
     @vite('resources/css/app.css')
     <title>Nextlevel</title>
     <link rel="icon" href="{{ asset('images/nextLevel_logo.png') }}" type="image/x-icon">
